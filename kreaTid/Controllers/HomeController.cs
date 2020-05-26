c0481d7349f4cdd0e21df29b681c74761ec65718
Dorthea <dorthea@scandicmedia.com>
2020-05-26 10:30:46 +0200

    Dot Html Index Css Partial Views

--- kreaTid/Controllers/HomeController.cs
+++ kreaTid/Controllers/HomeController.cs
@@ -6,22 +6,28 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using kreaTid.Models;
+using kreaTid.Data;
+using Microsoft.EntityFrameworkCore;
 
 namespace kreaTid.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly kreaTidContext _context;
+
+        public HomeController(kreaTidContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        // GET: Products
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var kreaTidContext = _context.Product.Include(p => p.Category).Include (p => p.ProductImgs);
+            return View(await kreaTidContext.ToListAsync());
         }
+
 
         public IActionResult Privacy()
         {
